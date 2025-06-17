ed371431556495f04da41f0e5205d993a2cf5e82
znack2 <vertex-3d@mail.ru>
2025-06-17 13:54:02 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: '/book_local/',
   server: {
     host: "::",
     port: 8080,
